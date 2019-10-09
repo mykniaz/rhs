406031599e55c59cf2db32ba2419ceb390d6b8ab
mykniaz <mykniaz@gmail.com>
2019-10-09 16:42:16 +0300

    fe add storybook viewports example

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,31 @@
 import { configure, addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+const newViewports = {
+	kindleFire2: {
+		name: 'Kindle Fire 2',
+		styles: {
+			width: '600px',
+			height: '963px',
+		},
+	},
+	kindleFireHD: {
+		name: 'Kindle Fire HD',
+		styles: {
+			width: '533px',
+			height: '801px',
+		},
+	},
+};
 
 addParameters({
 	viewport: {
-		viewports: INITIAL_VIEWPORTS,
-		defaultViewport: 'someDefault',
+		viewports: {
+			...INITIAL_VIEWPORTS,
+			...newViewports,
+		},
 	},
 });
+
+
+configure(require.context('../stories', true, /\.stories\.js$/), module);
